0e90aeeb7500f15cf1b086722e7499ca69d975d8
raihankobir895 <raihan895kobir@gmail.com>
2026-09-24 16:21:22 +0600

    Enhance MainMenu class with new methods and members
    Added constructor and destructor to MainMenu class, along with methods for creating and destroying controls and handling button commands.

--- include Folder/MainMenu.h
+++ include Folder/MainMenu.h
@@ -1,10 +1,24 @@
 #ifndef MAINMENU_H
 #define MAINMENU_H
 
+#include "GuiApp.h"
+
+// Main menu screen: title, "Start Quiz" and "Exit" buttons.
 class MainMenu
 {
 public:
-    void displayMenu();
+    MainMenu();
+    ~MainMenu();
+
+    void create(HWND parent);        // build the controls inside `parent`
+    void destroy();                  // remove the controls (safe to call twice)
+    void handleCommand(int controlId); // react to a button click
+
+private:
+    HWND titleLabel;
+    HWND subtitleLabel;
+    HWND startButton;
+    HWND exitButton;
 };
 
 #endif
